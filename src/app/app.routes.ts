d5a1f8310f83f610d37eeca51737a2b08857f806
Leonardo Rigo <leonardoluisorlandorigo@gmail.com>
2025-08-23 18:49:31 -0300

    corrigi o problema de redirecionamento de paginas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { signupComponent } from './pages/signup/signup.component';
+import { UserComponent } from './pages/user/user.component';
+import { AuthGuard } from './services/auth-guard.service';
 
 export const routes: Routes = [
     {
@@ -10,5 +12,12 @@ export const routes: Routes = [
         {
         path: "signup",
         component: signupComponent
+    },
+     {
+        path: "user",
+        component: UserComponent,
+        canActivate: [AuthGuard]
     }
+
+
 ];
